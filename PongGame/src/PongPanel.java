c0c19834c4ed8d31eed543780b5155f574fb85b5
JamieFoley <jamie.foley97@gmail.com>
2023-12-11 02:03:35 +1300

    Added Event listener methods to PongPanel task

--- PongGame/src/PongPanel.java
+++ PongGame/src/PongPanel.java
@@ -1,4 +1,31 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-public class PongPanel {
 
+public class PongPanel extends JFrame implements ActionListener, KeyListener {
+	public PongPanel() {
+		setBackground(Color.BLACK);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent event) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+	}
+	
+	
 }
