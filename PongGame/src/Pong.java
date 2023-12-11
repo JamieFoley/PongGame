ebf9f953ffd3ccd3c35583009a803aac3a5e7d58
JamieFoley <jamie.foley97@gmail.com>
2023-12-11 14:21:57 +1300

    Added graphics to JPanel. Dotted line is instantiated with new paintComponent and paintDottedLine methods.
    added timer to redraw game components every few moments. Redraw time
    determined by TIMER_DELAY constant.

--- PongGame/src/Pong.java
+++ PongGame/src/Pong.java
@@ -11,12 +11,21 @@ public class Pong extends JFrame  {
 		setTitle(WINDOW_TITLE);
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setResizable(false);
+		add(new PongPanel());
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	public static void main(String[] args) {
-		new Pong();		
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			
+            public void run() {
+            	
+            	new Pong();
+            }
+            
+        });
 		
 	}
 
--- PongGame/src/PongPanel.java
+++ PongGame/src/PongPanel.java
@@ -1,16 +1,43 @@
 import java.awt.Color;
-import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
-
-public class PongPanel extends JFrame implements ActionListener, KeyListener {
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+	
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
+	
 	public PongPanel() {
-		setBackground(Color.BLACK);
+		
+		setBackground(BACKGROUND_COLOUR);
+		
+		Timer timer = new Timer(TIMER_DELAY, this);
+		
+		
+		timer.start();
 	}
-
+	
+	public void update() {
+		
+	}
+	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth()/2, 0, getWidth()/2, getHeight());
+		g2d.dispose();
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent event) {
 	}
@@ -25,7 +52,14 @@ public class PongPanel extends JFrame implements ActionListener, KeyListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent event) {
+		update();
+		repaint();
 	}
 	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		paintDottedLine(g);
+	}
 	
 }
